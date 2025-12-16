b50fd12f8313b4fb4df34ef9afdf1b9c9938d31c
Olaleye Blessing <Olayinkablexxy@gmail.com>
2025-12-16 20:28:15 +0100

    Resolve TS error and remove storybook files

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -3,6 +3,6 @@ export const tokenInfo: { [key: string]: { symbol: string; decimals: number } } 
   "0xddafbb505ad214d7b80b1f830fccc89b60fb7a83": { symbol: "USDC", decimals: 6 },
   "0xa555d5344f6fb6c65da19e403cb4c1ec4a1a5ee3": { symbol: "BREAD", decimals: 18 },
   "0x4ecaba5870353805a9f068101a40e0f32ed605c6": { symbol: "DAI", decimals: 18 },
-  // Gnosis doesn't have native USDT, this is a common wrapped version
+  // @ts-expect-error Gnosis doesn't have native USDT, this is a common wrapped version 
   "0x4ecaba5870353805a9f068101a40e0f32ed605c6": { symbol: "USDT", decimals: 6 },
 }
