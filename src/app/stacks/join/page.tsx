408e8b053a2069d4b11bcbef00c7169066bbd2d5
Olaleye Blessing <Olayinkablexxy@gmail.com>
2026-01-07 20:42:22 +0100

    Redirect to circle detail page after joining the circle

--- src/app/stacks/join/page.tsx
+++ src/app/stacks/join/page.tsx
@@ -79,7 +79,7 @@ export default function Page() {
 
 			alert("Invitation Accepted!");
 			// TODO: Redirect to the stack page using its ID.
-			router.push("/");
+			router.push(`/stacks/${circleId}`);
 		} catch (error) {
 			console.log("__ ERROR REDEEM __", error);
 		} finally {
