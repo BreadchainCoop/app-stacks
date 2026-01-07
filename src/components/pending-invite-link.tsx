2e0f7395b273b359d6bc2468e617d6a274648b75
Olaleye Blessing <Olayinkablexxy@gmail.com>
2026-01-07 22:00:52 +0100

    Improve UI whenever there is a copy action

--- src/components/pending-invite-link.tsx
+++ src/components/pending-invite-link.tsx
@@ -1,8 +1,7 @@
 import { Body } from "@breadcoop/ui";
 import { CopyIcon } from "@phosphor-icons/react/ssr";
 import { cn } from "../lib/utils";
-import { ReactNode } from "react";
-import { copyToClipboard } from "@/utils/copy-to-clipboard";
+import CopyButton from "./copy-button";
 
 const PendingInviteLink = ({
 	link,
@@ -24,13 +23,15 @@ const PendingInviteLink = ({
 				{label}
 			</span>
 			<span className="bg-paper-1 px-4 flex-2 truncate">{link}</span>
-			<button
-				onClick={() => copyToClipboard(link)}
+			<CopyButton
+				textToCopy={link}
+				checkedIconSize={16}
+				varaint="icon-text"
 				className="border-x-0! border-transparent! px-3 flex items-center justify-center gap-2.5 text-paper-main bg-primary-blue shrink-0"
 			>
 				<CopyIcon size={16} />
 				<span>Copy</span>
-			</button>
+			</CopyButton>
 		</Body>
 	);
 };
