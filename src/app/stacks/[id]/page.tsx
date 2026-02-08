d3b2add5f1ac2c19c67954f01b6b9636394d558b
Olaleye Blessing <Olayinkablexxy@gmail.com>
2026-02-08 21:07:25 +0100

    Update metadata

---
+++ src/app/stacks/[id]/page.tsx
@@ -0,0 +1,24 @@
+import { generateMetadata as _generateMetadata } from "@/utils/metadata";
+import { Metadata } from "next";
+
+export async function generateMetadata({ 
+	params 
+}: { 
+	params: Promise<{ id: string }> 
+}): Promise<Metadata> {
+	const { id } = await params;
+	
+	return _generateMetadata({
+		title: "View Stack - Bread Cooperative",
+		description: "Access your stack and save money with your friends.",
+		url: `/stacks/${id}`,
+	});
+}
+
+const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
+	const { id } = await params;
+
+	return <>{id}</>;
+};
+
+export default Page;
