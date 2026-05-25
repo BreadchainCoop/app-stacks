603a60e020404bd41058a027e56f3307662576a2
Francisco Rolotti <102830313+franrolotti@users.noreply.github.com>
2026-05-25 14:22:19 +0200

    fix: correct inflated total BREAD stacked

--- src/hooks/use-user-circles-list.ts
+++ src/hooks/use-user-circles-list.ts
@@ -25,6 +25,7 @@ const parseCircleData = (
 
   const circle = {
     ...c.circleInfo,
+    completedRounds: c.completedRounds,
     totalMember: Number(totalRounds),
     id: c.circleId,
     status: formattedStatus.status,
