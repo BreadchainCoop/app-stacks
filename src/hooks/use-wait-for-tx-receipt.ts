f9728cdf767be75891d048f5178a3bbfdc871bde
Olaleye Blessing <Olayinkablexxy@gmail.com>
2026-07-08 10:07:24 +0100

    Pin waitForTransactionReceipt to the default chain

--- src/hooks/use-wait-for-tx-receipt.ts
+++ src/hooks/use-wait-for-tx-receipt.ts
@@ -1,10 +1,14 @@
 import { Address } from "viem";
 import { waitForTransactionReceipt } from "@wagmi/core";
 import { wagmiConfig } from "@/components/providers/web3";
+import { getDefaultChainId } from "@/utils/chain";
 
 export const useWaitForTxReceipt = () => {
   const waitForTxReceipt = async (hash: Address) => {
-    await waitForTransactionReceipt(wagmiConfig, { hash });
+    await waitForTransactionReceipt(wagmiConfig, {
+      hash,
+      chainId: getDefaultChainId(),
+    });
   };
 
   return { waitForTxReceipt };
